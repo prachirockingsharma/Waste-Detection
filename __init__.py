fbd609419c948b4f2e22e05b88f14418de21b7bc
Prachi sharma <91550275+prachirockingsharma@users.noreply.github.com>
2025-07-27 03:48:48 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,9 @@
+import os
+TEST_ROOT = os.path.dirname(file_)  # root of test folder
 
+ANNOTATIONS = _TEST_ROOT + '/data/test_annotations.json'
+IMG_DIR = _TEST_ROOT + '/data/'
+INDICES = _TEST_ROOT + '/data/indices.json'
+
+TRAIN_CLASS = _TEST_ROOT + '/data/train_class.pkl'
+VAL_CLASS = _TEST_ROOT + '/data/val_class.pkl'
